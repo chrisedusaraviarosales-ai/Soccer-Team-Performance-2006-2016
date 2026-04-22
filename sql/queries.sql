4a7dc05bff900aca6afa1f252e823956c785d0af
dereksmitha22 <dereksmitha@gmail.com>
2026-04-21 21:15:17 -0400

    Add SQL queries to queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,17 @@
+-- ============================================================
+
+-- Queries
+
+SELECT * FROM Teams;
+SELECT * FROM Players;
+SELECT * FROM Matches;
+SELECT * FROM Scores;
+SELECT * FROM Standings;
+SELECT * FROM Coaches;
+SELECT * FROM Sponsors;
+SELECT * FROM Seasons;
+SELECT * FROM PlayerStats;
+SELECT * FROM TeamStats;
+SELECT * FROM MatchStats;
+SELECT * FROM Attendance;
+SELECT * FROM Performance;
